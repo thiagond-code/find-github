052275e7c35432dd38c51ed50b757d364828517e
Thiago Nogueira <thiagond360@gmail.com>
2026-05-22 20:40:39 +0000

    chore(app): add Vercel adapter for deployment

--- code/astro.config.mjs
+++ code/astro.config.mjs
@@ -5,6 +5,8 @@ import react from '@astrojs/react';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
@@ -17,5 +19,7 @@ export default defineConfig({
     schema: {
       GITHUB_API_PAT: envField.string({ context: 'server', access: 'secret' })
     }
-  }
+  },
+
+  adapter: vercel()
 });
